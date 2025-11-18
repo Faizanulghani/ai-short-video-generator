a40cf7f56441c5ffe2d3d7220b1094b3a1605f1e
Faizanulghani <faizanulghani1@gmail.com>
2025-11-18 12:11:42 +0500

    Sidenav created

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,10 +1,16 @@
 import Header from "./_components/Header";
+import SideNav from "./_components/SideNav";
 
 const DashboardLayout = ({ children }) => {
   return (
     <div>
-      <Header />
-      {children}
+      <div className="hidden md:block h-screen bg-white fixed mt-[65px]">
+        <SideNav />
+      </div>
+      <div>
+        <Header />
+        <div className="md:ml-64">{children}</div>
+      </div>
     </div>
   );
 };
