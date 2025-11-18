fa3d82242846cda9637225e6769cd827b10cefa6
Faizanulghani <faizanulghani1@gmail.com>
2025-11-18 11:52:55 +0500

    dashboard header ready

---
+++ app/dashboard/_components/Header.jsx
@@ -0,0 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+
+const Header = () => {
+  return (
+    <div className="p-3 px-5 flex items-center justify-between shadow-md">
+      <div className="flex gap-3 items-center">
+        <Image src="/logo.png" alt="logo" width={50} height={50}></Image>
+        <h2 className="font-bold text-xl">Ai Short Vid</h2>
+      </div>
+      <div className="flex gap-3 items-center">
+        <Button>Dashboard</Button>
+        <UserButton />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
