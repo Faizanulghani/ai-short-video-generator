9f9b61d4f8e0fe9369a295a32b0ce23a83e62b98
Faizanulghani <faizanulghani1@gmail.com>
2025-11-19 12:32:33 +0500

    Select topic added

---
+++ app/dashboard/create-new/_components/SelectTopics.jsx
@@ -0,0 +1,57 @@
+"use client";
+
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { useState } from "react";
+
+const SelectTopics = ({ onUserSelect }) => {
+  const options = [
+    "Custom Prompt",
+    "Random AI Story",
+    "Scary Story",
+    "Historical Facts",
+    "Bed Time Story",
+    "Motivational",
+    "Fun Facts",
+  ];
+  const [selectedOptions, setSelectedOptions] = useState("");
+  return (
+    <div>
+      <h2 className="font-bold text-2xl text-primary">Content</h2>
+      <p className="text-gray-500">What is the topic of your video</p>
+      <Select
+        onValueChange={(value) => {
+          setSelectedOptions(value);
+          value != "Custom Prompt" && onUserSelect("topic", value);
+        }}
+      >
+        <SelectTrigger className="w-full mt-2 p-6 text-lg">
+          <SelectValue placeholder="Content Type" />
+        </SelectTrigger>
+        <SelectContent>
+          {options.map((option, index) => (
+            <SelectItem value={option} key={index}>
+              {option}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+
+      {selectedOptions == "Custom Prompt" && (
+        <Textarea
+          className="mt-3"
+          onChange={(e) => onUserSelect("topic", e.target.value)}
+          placeholder="Write prompt on which you want to generate video"
+        />
+      )}
+    </div>
+  );
+};
+
+export default SelectTopics;
--- app/dashboard/create-new/page.jsx
+++ app/dashboard/create-new/page.jsx
@@ -1,5 +1,24 @@
+"use client";
+
+import { useState } from "react";
+import SelectTopics from "./_components/SelectTopics";
+
 const CreateNew = () => {
-  return <div>CreateNew</div>;
+  let [formData, setFormData] = useState([]);
+  const onHandleInputChange = (fieldName, fieldValue) => {
+    console.log(fieldName, fieldValue);
+  };
+  return (
+    <div className="md-px-20">
+      <h2 className="font-bold text-4xl text-primary text-center">
+        Create New
+      </h2>
+
+      <div className="mt-10 shadow-md p-10">
+        <SelectTopics onUserSelect={onHandleInputChange} />
+      </div>
+    </div>
+  );
 };
 
 export default CreateNew;
